067b7faa94657dbbbb88d4861329f1f9e3f43109
Omkar <omkar1999.14@gmail.com>
2020-11-26 02:14:39 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { authConfig } from './sign.config';
+import { filter } from 'minimatch';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sign-app';
+  
+  title="sign-app"
+  constructor(private oauthService:OAuthService){
+    this.configureSingleSignOn();
+
+  }
+  configureSingleSignOn(){
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  login(){
+    this.oauthService.initImplicitFlow();
+
+  }
+  logout(){
+    this.oauthService.logOut();
+
+  }
+  get token(){
+    let claims:any = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
 }
